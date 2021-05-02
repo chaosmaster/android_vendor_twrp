56b5c49dd0cc94fcf1c9583530322b8f32696997
nebrassy <nebras30@gmail.com>
2021-05-02 17:14:51 +0200

    add twrp cmdline for recovery as boot devices
    Change-Id: Ib8c6f4f824463dffbbfd7505b369a97a6b6d2db0

--- config/BoardConfigTWRP.mk
+++ config/BoardConfigTWRP.mk
@@ -1,2 +1,8 @@
 include vendor/twrp/config/BoardConfigKernel.mk
 include vendor/twrp/config/BoardConfigSoong.mk
+
+ifeq ($(BOARD_USES_RECOVERY_AS_BOOT),true)
+GENERIC_KERNEL_CMDLINE += twrpfastboot=1
+endif
+
+
